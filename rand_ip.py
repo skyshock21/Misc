f8a0808abd3510ccebe55c0c4c24c516cf4a0a47
Paul Eubanks <pauleubanks@gmail.com>
2019-01-20 10:10:18 -0500

    refactoring if checks in main

--- rand_ip.py
+++ rand_ip.py
@@ -6,10 +6,8 @@
 # TO-DO:
 # create flag for generating multiple IPs (-n 10)
 # add_argument('-n', '--number', action='store', dest='number', type=int, default=1, help='number of IPs')
-# refactor first if check in main to check for null flags
-# as if not len(sys.argv) > 1
 
-import random, struct, socket, argparse
+import random, struct, socket, argparse, sys
 from random import getrandbits
 
 # CLI arguments
@@ -27,9 +25,11 @@ def rand_ipv6():
     return socket.inet_ntop(socket.AF_INET6, struct.pack('>QQ', getrandbits(64), getrandbits(64)))
 
 if __name__ == "__main__":
-    if args.ipv4:
+    if not len(sys.argv) > 1:
         print(rand_ipv4())
     elif args.ipv6:
         print(rand_ipv6())
+    elif args.ipv4:
+        print(rand_ipv4())
     else:
         print(rand_ipv4())
