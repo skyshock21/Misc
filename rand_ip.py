e6a6d7df0b95b490c83eb0ba484e2f0b5ebb8d2f
Paul Eubanks <pauleubanks@gmail.com>
2019-01-21 12:26:23 -0500

    adding support for multiple address output via -n flag

--- rand_ip.py
+++ rand_ip.py
@@ -15,6 +15,7 @@ parser = argparse.ArgumentParser(description='Returns a random IP address.')
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-v4", "--ipv4", action="store_true")
 group.add_argument("-v6", "--ipv6", action="store_true")
+parser.add_argument("-n", "--number", type=int, help="Number of IPs to output")
 args = parser.parse_args()
 
 # Function Defs
@@ -24,12 +25,26 @@ def rand_ipv4():
 def rand_ipv6():
     return socket.inet_ntop(socket.AF_INET6, struct.pack('>QQ', getrandbits(64), getrandbits(64)))
 
+# Main logic - Check if no arguments were passed, and output an ipv4 address (default)
+# if a value was specified as a flag -n, check if -v6 flag also passed and output -v6 format n times
+# if a value was specified as a flag -n, and -v6 flag not set, output -v4 format, n times
+# if no value was specified as -n, check if -v4 or -v6 flag were specified and output in that
+# respective format
+# if no flags specified, output a single v4 address (default) 
 if __name__ == "__main__":
     if not len(sys.argv) > 1:
         print(rand_ipv4())
+    elif args.number:
+        if args.ipv6:
+            for i in range(args.number):
+                print(rand_ipv6())
+        else:
+            for i in range(args.number):
+                print(rand_ipv4())
     elif args.ipv6:
         print(rand_ipv6())
     elif args.ipv4:
         print(rand_ipv4())
     else:
         print(rand_ipv4())
+
